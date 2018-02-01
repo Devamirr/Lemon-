9bb4558ef1c46f0b0fed4a0466ff87275ea9c251
Devamirr <amir58@live.nl>
2018-02-01 23:14:35 +0100

    added whole new filter and navigation layout

--- js/base.js
+++ js/base.js
@@ -2,52 +2,70 @@ $(window).scroll(function () {
     $(".back-fade").css("opacity", 1 - $(window).scrollTop() / 250);
 });
 
-/* ============================================================
- * bootstrap-portfilter.js for Bootstrap v2.3.1
- * https://github.com/geedmo/portfilter
- * ============================================================*/
-! function (d) {
-    var c = "portfilter";
-    var b = function (e) {
-        this.$element = d(e);
-        this.stuff = d("[data-tag]");
-        this.target = this.$element.data("target") || ""
-    };
-    b.prototype.filter = function (g) {
-        var e = [],
-            f = this.target;
-        this.stuff.fadeOut("fast").promise().done(function () {
-            d(this).each(function () {
-                if (d(this).data("tag") == f || f == "all") {
-                    e.push(this)
-                }
-            });
-            d(e).show()
-        })
-    };
-    var a = d.fn[c];
-    d.fn[c] = function (e) {
-        return this.each(function () {
-            var g = d(this),
-                f = g.data(c);
-            if (!f) {
-                g.data(c, (f = new b(this)))
+
+(function($, window, document, undefined) {
+    'use strict';
+
+    // init cubeportfolio
+    $('#js-grid-masonry-projects').cubeportfolio({
+        filters: '#js-filters-masonry-projects',
+        loadMore: '#js-loadMore-masonry-projects',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'quicksand',
+        gapHorizontal: 35,
+        gapVertical: 25,
+        gridAdjustment: 'responsive',
+        mediaQueries: [{
+            width: 1500,
+            cols: 5,
+        }, {
+            width: 1100,
+            cols: 4,
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 480,
+            cols: 2,
+            options: {
+                caption: '',
+                gapHorizontal: 25,
+                gapVertical: 10,
             }
-            if (e == "filter") {
-                f.filter()
-            }
-        })
-    };
-    d.fn[c].defaults = {};
-    d.fn[c].Constructor = b;
-    d.fn[c].noConflict = function () {
-        d.fn[c] = a;
-        return this
-    };
-    d(document).on("click.portfilter.data-api", "[data-toggle^=portfilter]", function (f) {
-        d(this).portfilter("filter")
-    })
-}(window.jQuery);
+        }],
+        caption: 'zoom',
+        displayType: 'fadeIn',
+        displayTypeSpeed: 100,
 
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: true,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
 
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: true,
+        singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
 
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+    });
+})(jQuery, window, document);
