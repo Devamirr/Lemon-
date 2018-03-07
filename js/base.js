874d336044577891c7ec9bb392dfd80ef19f04d7
Devamirr <amir58@live.nl>
2018-03-07 15:34:08 +0100

    added on sroll remove splashcreen

--- js/base.js
+++ js/base.js
@@ -1,6 +1,11 @@
 $(window).scroll(function () {
     $(".back-fade").css("opacity", 1 - $(window).scrollTop() / 250);
     $(".fade-out-on-scroll").css("opacity", 1 - $(window).scrollTop() / 150);
+    
+    if ($(this).scrollTop()>0)
+     {
+        $('.splash-fade').slideUp(900);
+     }
 });
 
 $('.enter_link').click(function () {
