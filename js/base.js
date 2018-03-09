7f7a62ed1574439e9bd03b29a1b43df2d1246126
Devamirr <amir58@live.nl>
2018-03-09 17:16:10 +0100

    added tweenmax for intro

--- js/base.js
+++ js/base.js
@@ -17,3 +17,36 @@ if (!sessionStorage.isVisited) {
     $(this).parent('#splashscreen').slideUp(500);
 }
 
+var aFx = 70,
+    trF = 4;  
+$('.container').on('mousemove touchmove',function(e){   
+  var cH = $('.container').innerHeight(),
+      cW = $('.container').innerWidth(),
+      eX = (e.originalEvent.type === 'touchmove') ? e.originalEvent.touches[0].pageX  : e.offsetX,
+      eY = (e.originalEvent.type === 'touchmove') ? e.originalEvent.touches[0].pageY  : e.offsetY;
+
+  $.each($('.photo'), function(i,el){
+ //console.log(((eY - cH / 2) / aFx) - (i*2));
+    TweenMax.set( $(el),{
+      transformOrigin: ((eX / (cW*trF) / 100 * 10000) + (trF * 10))+'% '+((eY / (cH*trF) / 100 * 10000) + (trF * 10))+'%',
+      transformPerspective: 1000 + (i * 500)});
+    TweenMax.to( $(el), 0.5, { 
+      rotationX: ((eY - cH / 2) / aFx) - i * 2, 
+      rotationY: ((eX - cW / 2) / aFx * -1) - i * 2,
+      y:(eY - (cH / 2)) / (70 - i * 20),  
+      x:(eX - (cW / 2)) / (70 - i * 20)
+    });
+  });
+
+}).on('mouseout touchend',function(e){
+  $.each($('.photo'),function(i,el){
+    TweenMax.to( $(el), 1, {
+      delay:.2,
+      y:0,
+      x:0,
+      rotationX: 0,
+      rotationY: 0,
+      transformPerspective:'1500' 
+    });
+  });
+});
